43db53a931d80e21596940ba636aca04aa6c81b7
ringinrp <ringinrp21@gmail.com>
2023-12-18 12:55:59 +0700

    update: route

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
+Route::prefix('admin')->name('admin.')->middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
