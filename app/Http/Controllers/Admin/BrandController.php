141363d897c37bd84f0c03913ba614fa3c505a7e
ringinrp <ringinrp21@gmail.com>
2023-12-22 09:17:56 +0700

    update: brand

--- app/Http/Controllers/Admin/BrandController.php
+++ app/Http/Controllers/Admin/BrandController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Brand;
+use Illuminate\Support\Str;
+use Yajra\DataTables\DataTables;
+use App\Http\Requests\BrandRequest;
 
 class BrandController extends Controller
 {
@@ -12,7 +16,30 @@ class BrandController extends Controller
      */
     public function index()
     {
-        //
+        //script untuk datatables, AJAX
+        if (request()->ajax()) {
+            $query = Brand::query();
+
+            return DataTables::of($query)
+                ->addColumn('action', function ($brand) {
+                    return '
+                        <a class="block w-full px-2 py-1 mb-1 text-xs text-center text-white transition duration-500 bg-gray-700 border border-gray-700 rounded-md select-none ease hover:bg-gray-800 focus:outline-none focus:shadow-outline" 
+                            href="' . route('admin.brands.edit', $brand->id) . '">
+                            Sunting
+                        </a>
+                        <form class="block w-full" onsubmit="return confirm(\'Apakah anda yakin?\');" -block" action="' . route('admin.brands.destroy', $brand->id) . '" method="POST">
+                        <button class="w-full px-2 py-1 text-xs text-white transition duration-500 bg-red-500 border border-red-500 rounded-md select-none ease hover:bg-red-600 focus:outline-none focus:shadow-outline" >
+                            Hapus
+                        </button>
+                            ' . method_field('delete') . csrf_field() . '
+                        </form>';
+                })
+                ->rawColumns(['action'])
+                ->make();
+        }
+
+        //script untuk return halaman view brand
+        return view('admin.brands.index');
     }
 
     /**
